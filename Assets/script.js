3d9a9a007762a4a97d0ec9c0e8507ede4dcbc17c
David Hybner <dah1998@yahoo.com>
2022-09-07 15:26:31 -0400

    Added Js
    added js to append list to page and added styling for these elements

--- Assets/script.js
+++ Assets/script.js
@@ -1,6 +1,19 @@
 var buttonPress = document.querySelector('#Btn');
+var Product =$("#item");
+var Amount =$('#Amount');
+var Add= $('#Addhere');
 
-
-buttonPress.addEventListener("click", function () {  
+buttonPress.addEventListener("click", function (event) {  
     console.log('pressed');
-})
+    if(Product.val() == "" || Amount.val() == ""){
+       window.alert('Please Fill out all Fields');
+}
+
+    var List =$('<h1>');
+    console.log(Product.val()+" x"+Amount.val())
+    List.text(Product.val()+" x"+Amount.val());
+    List.attr('id','AddProduct');
+    Add.append(List);
+
+
+});
