b185945f64e863768a5aed1c48d26400e7f2c884
David Hybner <dah1998@yahoo.com>
2022-09-08 03:49:49 -0400

    Update js
    Added local storage to keep list elements. Added event listeners for more buttons in the settings menu and added moment.js

--- Assets/script.js
+++ Assets/script.js
@@ -1,19 +1,120 @@
 var buttonPress = document.querySelector('#Btn');
-var Product =$("#item");
-var Amount =$('#Amount');
-var Add= $('#Addhere');
+var buttonLight = document.querySelector('#Lights');
+var Settings = document.querySelector('#Settings');
+var Pointer = document.querySelector('#mouse');
+var Timer = document.querySelector('#Time');
+var TimeSpot = document.querySelector('#time');
+var Product = $("#item");
+let list = document.querySelector("ul");
+var Amount = document.getElementById('Amount');
+var Add = $('#Addhere');
+var ClearHistory=document.querySelector('#History');
+var trial = 0;
+var trial2=0;
+var AllCount = document.getElementById('Count');
+var All= 0;
+var flip = true;
 
-buttonPress.addEventListener("click", function (event) {  
-    console.log('pressed');
-    if(Product.val() == "" || Amount.val() == ""){
-       window.alert('Please Fill out all Fields');
+var Historyc=$(JSON.parse(localStorage.getItem("list")));
+addEntry();
+function addEntry() {
+    Historyc.sort();
+    for (var i = 0; i < Historyc.length; i++){
+        if(Historyc[i] === Historyc[i -1]){
+            Historyc.splice(i, 1);
+            i--;
+        }
+    }
+    CreateList();
+}
+function CreateList(){
+    Add.children().remove();
+    for(var i = 0; i <Historyc.length; i++){
+    var List = $('<li>');
+    List.text(Historyc[i]);
+    List.attr('id', 'AddProduct');
+    Add.append(List);
+    All=Historyc.length;
+    AllCount.innerHTML = All;
+    }
+  
 }
 
-    var List =$('<h1>');
-    console.log(Product.val()+" x"+Amount.val())
-    List.text(Product.val()+" x"+Amount.val());
-    List.attr('id','AddProduct');
-    Add.append(List);
 
+buttonPress.addEventListener("click", function (event) {
+    console.log('pressed');
+   
+    if (Product.val() == "" || Amount.value == "") {
+        window.alert('Please Fill out all Fields');
+    } else {
+        M.toast({ html: 'Item Added!' })
+        var temp=(Product.val()+ " x"+ Amount.value);
+        Historyc.push(temp);
+        localStorage.setItem('list',JSON.stringify(Historyc));
+       
+        addEntry();
+    }
 
 });
+
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+
+Pointer.addEventListener('click', function (event) {
+
+    if (trial == 0) {
+        document.body.style.cursor = 'crosshair';
+        trial = 1;
+
+    } else if (trial == 1) {
+        document.body.style.cursor = 'cell';
+        trial = 2;
+
+    }
+    else if (trial == 2) {
+        document.body.style.cursor = 'grab';
+        trial = 3;
+
+    }
+    else if (trial == 3) {
+        document.body.style.cursor = 'pointer';
+        trial = 4;
+
+    }
+    else {
+        document.body.style.cursor = 'default';
+        trial = 0;
+    }
+
+
+
+})
+
+
+// Dark/light mode 
+buttonLight.addEventListener('click', function (event) {
+   
+    var element = document.body;
+   
+
+
+  
+    element.classList.toggle('LightMode');
+    
+})
+
+Timer.addEventListener('click', function(event){
+    
+    window.setInterval(function () {
+   TimeSpot.innerHTML=moment().format('LTS');
+}, 1000);
+
+})
+
+
+ClearHistory.addEventListener('click',function () {
+    localStorage.clear();
+    location.reload();
+  })
